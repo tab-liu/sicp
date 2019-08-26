2bb5b2353292b9b3526ad819e0abdad443e00faf
tab <dearhange@icloud.com>
2019-08-26 11:29:26 +0800

    update

--- chapter1/sicp.1.22.scm
+++ chapter1/sicp.1.22.scm
@@ -1,4 +1,4 @@
-; Exercise 1.22
+; Exercise 1.22 1.23 1.24
 (load "sicp.1.21.scm")
 (define (prime? n)
   (= n (smallest-divisor n)))
@@ -16,5 +16,26 @@
   (display " *** ")
   (display elapsed-time))
 
-(timed-prime-test 19)
-(timed-prime-test 1999)
+(define (search-for-primes n)
+  (define (next n)
+    (if (even? n) (1+ n) (+ n 2)))
+  (define st (runtime))
+  (define (report-prime n)
+    (newline)
+    (display n)
+    (display " *** ")
+    (display (- (runtime) st)))
+
+  (define (search n c)
+    (cond ((= c 0)
+            (newline)
+            (display "DONE"))
+          ((prime? n)
+            (report-prime n)
+            (search (next n) (-1+ c)))
+          (else
+            (search (next n) c))))
+  (search n 300))
+
+; test
+(search-for-primes 1000000)
